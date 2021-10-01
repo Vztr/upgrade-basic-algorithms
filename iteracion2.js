faba2279e5dd187ad2c915a911a820f9e9b29aa3
Victor Zorrilla Brasal <victorzorrillabrasal@gmail.com>
2021-10-01 18:31:22 +0200

    IterationOne

---
+++ iteracion2.js
@@ -0,0 +1,74 @@
+//////// ITERACION 2 ////////
+
+//------------------- 2.1
+
+const character = {name: 'Jack Sparrow', age: 10};
+
+ character.age = 25;
+
+//------------------- 2.2
+
+ let firstName = 'Jon'; 
+ let lastName = 'Snow'; 
+ let age = 24;
+
+ console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.")
+
+ //------------------ 2.3
+
+ const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+console.log(toy1.price + toy2.price);
+
+ //------------------ 2.4
+
+ let globalBasePrice = 10000;
+ globalBasePrice = 25000;
+ 
+ console.log(globalBasePrice);
+ 
+ const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+ 
+ car1.finalPrice = car1.basePrice + globalBasePrice;
+ car2.finalPrice = car2.basePrice + globalBasePrice;
+ 
+ console.log (car1, car2);
+
+//////// ITERACION 3 ///////
+
+//------------------- 3.1
+
+let resultado = 10 * 5;
+console.log(resultado);
+
+//------------------- 3.2
+
+let resultado2 = 10 / 2;
+console.log(resultado2);
+
+//------------------- 3.3
+
+let resultado3 = 15 % 9;
+console.log(resultado3);
+
+//------------------- 3.4
+
+let y = 10;
+let z = 5;
+
+let x = y + z; 
+
+console.log(x);
+
+//------------------- 3.5
+
+let y = 10;
+let z = 5;
+
+let x = y * z;
+
+console.log(x);
+
+//////// ITERACION 4 ////////
