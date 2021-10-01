5c22c865d873b4362d76487c06300928e9465525
Victor Zorrilla Brasal <victorzorrillabrasal@gmail.com>
2021-10-01 18:34:10 +0200

    iterationThree

--- JS Basic Algorithms.js
+++ JS Basic Algorithms.js
@@ -55,3 +55,38 @@ console.log(toy1.price + toy2.price);
  car2.finalPrice = car2.basePrice + globalBasePrice;
  
  console.log (car1, car2);
+ 
+//////// ITERACION 3 ///////
+
+//------------------- 3.1
+
+let resultado = 10 * 5;
+console.log(resultado);
+
+//------------------- 3.2
+
+let resultado2 = 10 / 2;
+console.log(resultado2);
+
+//------------------- 3.3
+
+let resultado3 = 15 % 9;
+console.log(resultado3);
+
+//------------------- 3.4
+
+let y = 10;
+let z = 5;
+
+let x = y + z; 
+
+console.log(x);
+
+//------------------- 3.5
+
+let y = 10;
+let z = 5;
+
+let x = y * z;
+
+console.log(x);
